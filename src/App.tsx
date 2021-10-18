d47491184f757c64518f4cb7f7d68db414393d30
Pavel Chlebek <pavelchlebek990@gmail.com>
2021-10-18 14:08:25 +0200

    Starting React-Typescript repo

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import React from 'react';
+
+import logo from './logo.svg';
 
 function App() {
   return (
@@ -8,7 +10,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Palko <code>the programmer</code> is the greatest one!
         </p>
         <a
           className="App-link"
@@ -20,7 +22,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
