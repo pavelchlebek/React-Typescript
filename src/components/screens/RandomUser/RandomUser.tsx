a4718ba98215012b0a9d492daee8d8e5ee413ee5
pavelchlebek <pavelchlebek990@gmail.com>
2022-04-05 12:33:36 +0200

    Add useFatch.tsx - hook for fetching data + function for fatching data and setFunction callback

--- src/components/screens/RandomUser/RandomUser.tsx
+++ src/components/screens/RandomUser/RandomUser.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 
 import axios from 'axios';
 
+import useFetch2, { fetchDataSet } from '../../../hooks/useFetch';
 import classes from './RandomUser.module.css';
 import { User } from './User';
 
@@ -26,8 +27,9 @@ const flattenUser = (user: any) => {
 export const RandomUser: React.FC<TProps> = () => {
   const [users, setUsers] = React.useState<IUser[]>([])
   const [userCount, setUserCount] = React.useState(0)
+  const [someData, setSomeData] = React.useState()
 
-  // const { data, loading, error } = useFetch(`https://randomuser.me/api/?results=1`)
+  const { data, loading } = useFetch2(`https://randomuser.me/api/?results=1`)
 
   const fetchUsers = async (count: number) => {
     try {
@@ -43,6 +45,10 @@ export const RandomUser: React.FC<TProps> = () => {
 
   // console.log("hookUsers: ", hookUsers)
 
+  const handleFetch = () => {
+    fetchDataSet("https://randomuser.me/api/?results=1", (data) => setSomeData(data))
+  }
+
   return (
     <div className={classes.screen}>
       <div className={classes.user}>
@@ -52,7 +58,8 @@ export const RandomUser: React.FC<TProps> = () => {
             })
           : "No users fetched"}
       </div>
-      {/* <div className={classes.hookUser}>{JSON.stringify(data)}</div> */}
+      <div className={classes.hookUser}>{loading ? "loading...." : JSON.stringify(data)}</div>
+      <div className={classes.hookUser}>{!someData ? "loading...." : JSON.stringify(someData)}</div>
       <div className={classes.form}>
         Users count:
         <input
@@ -63,6 +70,13 @@ export const RandomUser: React.FC<TProps> = () => {
       </div>
 
       <button onClick={() => fetchUsers(userCount)}>Fetch User</button>
+      <button
+        onClick={() =>
+          fetchDataSet("https://randomuser.me/api/?results=1", (data) => setSomeData(data))
+        }
+      >
+        Fetch Date Set
+      </button>
       <div className={classes.display}>Weronika na mne mysli!!!</div>
     </div>
   )
--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -2,7 +2,8 @@ import React, { useEffect } from 'react';
 
 import axios from 'axios';
 
-const useFetch = async (url: string) => {
+// useFetch hook that is typically used for fetching initial data for cmp (eg. screen)
+const useFetch = (url: string) => {
   const [data, setData] = React.useState(null)
   const [loading, setLoading] = React.useState(false)
   const [error, setError] = React.useState(null)
@@ -25,4 +26,37 @@ const useFetch = async (url: string) => {
   return { data, loading, error }
 }
 
-export default useFetch
+// same hook as above but written using async / await
+const useFetch2 = (url: string) => {
+  const [data, setData] = React.useState(null)
+  const [loading, setLoading] = React.useState(false)
+  const [error, setError] = React.useState<any>(null)
+
+  useEffect(() => {
+    setLoading(true)
+    async function fetchData(url: string) {
+      try {
+        const response = await axios.get(url)
+        setData(response.data)
+      } catch (err) {
+        setError(err)
+      } finally {
+        setLoading(false)
+      }
+    }
+    fetchData(url)
+  }, [url])
+
+  return { data, loading, error }
+}
+export default useFetch2
+
+// fetch data function with callback fn that can be used to setState in the cmp
+export const fetchDataSet = async (url: string, setFunction: (data: any) => void) => {
+  try {
+    const response = await axios.get(url)
+    setFunction(response.data)
+  } catch (err) {
+    console.error("Request failed: ", err)
+  }
+}
